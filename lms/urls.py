a697058bb04528b15fc4fba68f4cae8525656bea
Lana Virina <lana.virina@gmail.com>
2025-01-14 21:29:15 +0300

    Add CRUD

---
+++ lms/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from lms.views import CourseViewSet, LessonUpdateAPIView, LessonRetrieveAPIView, LessonListAPIView, \
+    LessonCreateAPIView, LessonDestroyAPIView
+from rest_framework.routers import SimpleRouter
+from lms.apps import LmsConfig
+
+app_name = LmsConfig.name
+
+router = SimpleRouter()
+router.register('', CourseViewSet)
+
+urlpatterns = [
+    path('lessons/', LessonListAPIView.as_view(), name='lessons_list'),
+    path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_retrieve'),
+    path('lessons/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lessons/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+] + router.urls
--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
 
-# Create your views here.
+from lms.serializers import CourseSerializer, LessonSerializer
+
+from lms.models import Course, Lesson
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonCreateAPIView(generics.CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListAPIView(generics.ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateAPIView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyAPIView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
