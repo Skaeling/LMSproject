8e6b1d207be003cefda741367c7d74cf215d179f
Lana Virina <lana.virina@gmail.com>
2025-03-12 01:35:56 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -165,11 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = "/var/www/LMSproject/app/static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/LMSproject/app/staticfiles/",
-]
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
