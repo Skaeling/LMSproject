ebf9129fea2cb0368da3c6b6d2073e4fc7b44763
Lana Virina <lana.virina@gmail.com>
2025-03-12 01:55:49 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/admin/var/www/LMSproject/staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
